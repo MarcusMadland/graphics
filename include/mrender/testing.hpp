1cb4ede8c6db06a4d0d486e890484458e24944e6
marcusmadland <mrmaratcho@gmail.com>
2023-04-01 17:11:50 +0200

    Worked on the backend renderer to handle third party api setup

--- include/mrender/testing.hpp
+++ include/mrender/testing.hpp
@@ -1,1 +1,10 @@
-#include <bgfx/bgfx.h>
+#pragma once
+
+class Backend
+{
+public:
+    static void init(const int width, const int height, void* nativeWindow, void* nativeDisplay = nullptr);
+    static void shutdown();
+
+	static void render();
+};
--- src/mrender/testing.cpp
+++ src/mrender/testing.cpp
@@ -1,1 +1,77 @@
 #include "../include/mrender/testing.hpp"
+#include "../include/mrender/file_ops.hpp"
+
+#include <bgfx/bgfx.h>
+#include <bgfx/platform.h>
+#include <bx/math.h>
+#include <string>
+
+struct RendererData
+{
+};
+static RendererData* data;
+
+void Backend::init(const int width, const int height, void* nativeWindow, void* nativeDisplay)
+{
+    if (nativeWindow == nullptr)
+    {
+        return;
+    }
+      
+    printf("Init renderer");
+
+    bgfx::renderFrame(); // single threaded mode
+
+    // Platform Data
+    bgfx::PlatformData pd{};
+    pd.nwh = nativeWindow;
+    pd.ndt = nativeDisplay;
+
+    // Init bgfx
+    bgfx::Init bgfx_init;
+    bgfx_init.type = bgfx::RendererType::Count; // auto choose renderer
+    bgfx_init.resolution.width = width;
+    bgfx_init.resolution.height = height;
+    bgfx_init.resolution.reset = BGFX_RESET_VSYNC;
+    bgfx_init.platformData = pd;
+    bgfx::init(bgfx_init);
+
+    // Clear screen
+    bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x6495EDFF, 1.0f, 0);
+    bgfx::setViewRect(0, 0, 0, width, height);
+}
+
+void Backend::shutdown()
+{
+    bgfx::shutdown();
+}
+
+void Backend::render()
+{
+    bgfx::touch(0);
+    bgfx::dbgTextClear();
+   
+    bgfx::dbgTextPrintf(0, 0, 0x0f, "Press F1 to toggle stats.");
+    bgfx::dbgTextPrintf(
+        0, 1, 0x0f,
+        "Color can be changed with ANSI "
+        "\x1b[9;me\x1b[10;ms\x1b[11;mc\x1b[12;ma\x1b[13;mp\x1b[14;me\x1b[0m "
+        "code too.");
+    bgfx::dbgTextPrintf(
+        80, 1, 0x0f,
+        "\x1b[;0m    \x1b[;1m    \x1b[; 2m    \x1b[; 3m    \x1b[; 4m    \x1b[; "
+        "5m    \x1b[; 6m    \x1b[; 7m    \x1b[0m");
+    bgfx::dbgTextPrintf(
+        80, 2, 0x0f,
+        "\x1b[;8m    \x1b[;9m    \x1b[;10m    \x1b[;11m    \x1b[;12m    "
+        "\x1b[;13m    \x1b[;14m    \x1b[;15m    \x1b[0m");
+    const bgfx::Stats* stats = bgfx::getStats();
+    bgfx::dbgTextPrintf(
+        0, 2, 0x0f,
+        "Backbuffer %dW x %dH in pixels, debug text %dW x %dH in characters.",
+        stats->width, stats->height, stats->textWidth, stats->textHeight);
+ 
+    bgfx::setDebug(true ? BGFX_DEBUG_STATS : BGFX_DEBUG_TEXT);
+   
+    bgfx::frame();
+}
