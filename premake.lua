503fcb4de0e3b2adc3d9aa793188095149bf0b83
marcusmadland <mrmaratcho@gmail.com>
2023-04-09 11:19:05 +0200

    Added bgfx submodules

--- premake.lua
+++ premake.lua
@@ -1,3 +1,18 @@
+-- External third Party directories
+local BGFX_DIR = "third-party/bgfx"
+local BIMG_DIR = "third-party/bimg"
+local BX_DIR = "third-party/bx"
+
+function setBxCompat()
+	filter "action:vs*"
+		includedirs { path.join(BX_DIR, "include/compat/msvc") }
+	filter { "system:windows", "action:gmake" }
+		includedirs { path.join(BX_DIR, "include/compat/mingw") }
+	filter { "system:macosx" }
+		includedirs { path.join(BX_DIR, "include/compat/osx") }
+		buildoptions { "-x objective-c++" }
+end
+
 project "mrender"
 	kind "StaticLib"
 	language "C++"
@@ -17,3 +32,106 @@ project "mrender"
 	{
 		"include"
 	}
+
+	links { "bgfx", "bimg", "bx" }
+	filter "system:windows"
+		links { "gdi32", "kernel32", "psapi" }
+	filter "system:linux"
+		links { "dl", "GL", "pthread", "X11" }
+	filter "system:macosx"
+		links { "QuartzCore.framework", "Metal.framework", "Cocoa.framework", "IOKit.framework", "CoreVideo.framework" }
+	setBxCompat()
+
+project "bgfx"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++14"
+	exceptionhandling "Off"
+	rtti "Off"
+	defines "__STDC_FORMAT_MACROS"
+	files
+	{
+		path.join(BGFX_DIR, "include/bgfx/**.h"),
+		path.join(BGFX_DIR, "src/*.cpp"),
+		path.join(BGFX_DIR, "src/*.h"),
+	}
+	excludes
+	{
+		path.join(BGFX_DIR, "src/amalgamated.cpp"),
+	}
+	includedirs
+	{
+		path.join(BX_DIR, "include"),
+		path.join(BIMG_DIR, "include"),
+		path.join(BGFX_DIR, "include"),
+		path.join(BGFX_DIR, "3rdparty"),
+		path.join(BGFX_DIR, "3rdparty/dxsdk/include"),
+		path.join(BGFX_DIR, "3rdparty/khronos")
+	}
+	filter "action:vs*"
+		defines "_CRT_SECURE_NO_WARNINGS"
+		excludes
+		{
+			path.join(BGFX_DIR, "src/glcontext_glx.cpp"),
+			path.join(BGFX_DIR, "src/glcontext_egl.cpp")
+		}
+	filter "system:macosx"
+		files
+		{
+			path.join(BGFX_DIR, "src/*.mm"),
+		}
+	setBxCompat()
+
+project "bimg"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++14"
+	exceptionhandling "Off"
+	rtti "Off"
+	files
+	{
+		path.join(BIMG_DIR, "include/bimg/*.h"),
+		path.join(BIMG_DIR, "src/image.cpp"),
+		path.join(BIMG_DIR, "src/image_gnf.cpp"),
+		path.join(BIMG_DIR, "src/*.h"),
+		path.join(BIMG_DIR, "3rdparty/astc-codec/src/decoder/*.cc")
+	}
+	includedirs
+	{
+		path.join(BX_DIR, "include"),
+		path.join(BIMG_DIR, "include"),
+		path.join(BIMG_DIR, "3rdparty/astc-codec"),
+		path.join(BIMG_DIR, "3rdparty/astc-codec/include"),
+	}
+	setBxCompat()
+
+project "bx"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++14"
+	exceptionhandling "Off"
+	rtti "Off"
+	defines "__STDC_FORMAT_MACROS"
+	files
+	{
+		path.join(BX_DIR, "include/bx/*.h"),
+		path.join(BX_DIR, "include/bx/inline/*.inl"),
+		path.join(BX_DIR, "src/*.cpp")
+	}
+	excludes
+	{
+		path.join(BX_DIR, "src/amalgamated.cpp"),
+		path.join(BX_DIR, "src/crtnone.cpp")
+	}
+	includedirs
+	{
+		path.join(BX_DIR, "3rdparty"),
+		path.join(BX_DIR, "include")
+	}
+	filter "configurations:Release"
+		defines "BX_CONFIG_DEBUG=0"
+	filter "configurations:Debug"
+		defines "BX_CONFIG_DEBUG=1"
+	filter "action:vs*"
+		defines "_CRT_SECURE_NO_WARNINGS"
+	setBxCompat()
